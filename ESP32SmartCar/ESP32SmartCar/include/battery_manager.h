f73f89969f671c7d728c2b45aaeee56c64ab1048
xy660 <73594017+xy660@users.noreply.github.com>
2025-10-31 20:47:44 +0800

    Delete ESP32SmartCar/ESP32SmartCar directory

--- ESP32SmartCar/ESP32SmartCar/include/battery_manager.h
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-
-float getVBat();
-void initBatteryManager();
-float sample_raw_vbat();
-void shutdown_and_deep_sleep(uint64_t sleep_us = 0);
-
-extern float prev_read_raw_vol; //上次读取到的原始电压值（没有经过校准）
